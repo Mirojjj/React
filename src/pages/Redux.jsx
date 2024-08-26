a6db8dc5160443642184570c991ddffd65fc5c07
mirojjj <raimiroj7@gmail.com>
2024-08-26 22:02:22 +0545

    vercel json

--- src/pages/Redux.jsx
+++ src/pages/Redux.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import OperationBtn from "../components/operationBtn.jsx";
+import OperationBtn from "../components/OperationBtn.jsx";
 import DigitBtn from "../components/DigitBtn.jsx";
 import {
   backSpace,
